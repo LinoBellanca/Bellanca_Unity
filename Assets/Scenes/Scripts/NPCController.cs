3030735c94206f2fc900863c06d761d2d8cb2fa0
LinoB <77000248+LinoBellanca@users.noreply.github.com>
2024-10-22 11:01:08 +0200

    Target + Speed retarget

---
+++ Assets/Scenes/Scripts/NPCController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+    private NavMeshAgent _agent;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _agent.SetDestination(_target.transform.position);
+
+    }
+
+    private void Update()
+    {
+        // Récupérer la vitesse de l'agent
+        float speed = _agent.velocity.magnitude;
+
+        // Mettre à jour le paramètre "Speed" dans l'Animator
+        _animator.SetFloat("Speed", speed);
+    }
+}
